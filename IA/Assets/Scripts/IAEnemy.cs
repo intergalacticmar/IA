ce2f3f677e369f0ad5c99573abd96c1384020598
María <120553496+intergalacticmar@users.noreply.github.com>
2024-01-26 10:46:51 +0100

    guardado 5

---
+++ IA/Assets/Scripts/IAEnemy.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Patroling,
+        Chasing
+    }
+
+    // Update is called once per frame
+    State currentState;
+
+    NavMeshAgent enemyAgent;
+    Transforms playersTransforms;
+
+    [SerializeField] Transform patrolAreaCenter;
+
+    [SerializeField] Vector2 patrolAreaSize;
+
+    [SerializeField] float visionRange = 15;
+
+    [SerializeField] float visionAngle = 90;
+
+    void Awake()
+    {
+        enemyAgent = GetComponent<NavMeshAgent>();
+        playersTransform = GameObject
+    }
+
+    void Update()
+    {
+        switch(currentState)
+        {
+            case State.Patrolling;
+                Patrol();
+            break;
+            case State.Chasing;
+                Chase();
+            break;
+        }
+    }
+    
+    void Patrol()
+    {
+        if(OnRange() == true)
+        {
+            currentState = State.Chasing;
+        }
+
+        if(enemyAgent.remainingDistance < 0,5f)
+        {
+            SetRandomPoint();
+        }
+    }
+
+    void Chase()
+    {
+        enemyAgent.destination = playersTransform.position;
+
+        if(OnRange() == false)
+        {
+            currentState = StatePatrolling;
+        }
+    }
+
+    void SetRandomPoint()
+    {
+        float randomX = Random.Range(-patrolAreaSize.x / 2, patrolAreaSize.x /2);
+        float randomZ = Random.Range(-patrolAreaSize.y / 2, patrolAreaSize.y /2);
+        Vector3 randomPoint = new Vector3(randomX, 0f, randomZ) + patrolAreaCenter.position;
+
+        enemyAgent.destination = randomPoint;
+    }
+
+    bool OnRange()
+    {
+        /*if(Vector3.Distance(transform.position, playersTransforms.position) <= visionRange)
+        {
+            return true;
+        }
+
+        return false;*/
+
+        Vector3 directionToPlayer = playersTransforms.position - transform.position;
+        float distanceToPlayer = dirextionToPlayer.magnitude;
+        float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+
+        if(distanceToPlayer <= visionRange && angleToPlayer < visionAngle * 0,5f)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.blue;
+        OnDrawGizmos.DrawWireCube(patrolAreaCenter.position, new Vector3(patrolAreaSize.x, 0, patrolAreaSize.y));
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRange);
+
+        Gizmos.color = Color.green;
+        Vector3 fovLine1 = Quaternion.AngleAxis(visionAngle * 0,5f, transform.up) * transform.forward * visionRange;
+        Vector3 fovLine2 = Quaternion.AngleAxis(-visionAngle * 0,5f, transform.up) * transform.forward * visionRange;
+        Gizmos.DrawLine(transform.position, transform.position + fovLine1);
+        Gizmos.DrawLine(transform.position, transform.position + fovLine2);
+    }
+}
